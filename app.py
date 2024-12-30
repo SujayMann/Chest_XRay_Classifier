5b585dfe3b0cc534103ba977c33058c6da2367ea
Sujay Mann <sujay2004mann@gmail.com>
2024-12-30 13:41:58 +0530

    model update

--- app.py
+++ app.py
@@ -5,28 +5,37 @@ from torchvision.models import resnet50
 from torchvision import transforms, datasets
 from PIL import Image
 
-torch.manual_seed(42)
+# torch.manual_seed(42)
 
-class CustomModel(nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.resnet = resnet50(pretrained=True)
-        self.resnet.fc = nn.Sequential(
-            nn.Linear(2048, 1),
-            nn.Sigmoid()
-        )
+# class CustomModel(nn.Module):
+#     def __init__(self):
+#         super().__init__()
+#         self.resnet = resnet50(pretrained=True)
+#         self.resnet.fc = nn.Sequential(
+#             nn.Linear(2048, 1),
+#             nn.Sigmoid()
+#         )
     
-    def forward(self, x):
-        return self.resnet(x)
+#     def forward(self, x):
+#         return self.resnet(x)
 
-model = CustomModel()
-state_dict = torch.load('model_resnet50.pth', map_location=torch.device('cpu'))
-model.load_state_dict(state_dict, strict=False)
+# model = CustomModel()
+# state_dict = torch.load('model_resnet50.pth', map_location=torch.device('cpu'))
+# model.load_state_dict(state_dict, strict=False)
+model = torch.load('model_resnet50.pth', map_location=torch.device('cpu'))
 
 labels = ['Normal', 'Pneumonia']
 
-st.title('Chest X-Ray Classifier')
-st.write('Pneumonia Detection from Chest X-rays')
+def info_page():
+    st.title("How to Use the App")
+    st.write("""
+        This app helps you upload chest x-rays and classify between `NORMAL` and `PNEUMONIA` using a pre-trained CNN model.
+        
+        **Steps**:
+        1. Navigate to the **Predict** page via the sidebar.
+        2. Upload an image using the **Browse files** button.
+        3. The model will predict if the x-ray shows pneumonia and display the result.
+    """)
 
 def preprocess_image(image):
     image_transform = transforms.Compose([
@@ -36,7 +45,10 @@ def preprocess_image(image):
     ])
     return image_transform(image).unsqueeze(0)
 
-def main():
+def prediction_page():
+
+    st.header('Image Upload and Prediction')
+
     img_path = st.file_uploader(label='Chest X-Ray', type=['png', 'jpg', 'jpeg'])
     if img_path is not None:
         st.image(img_path, caption='Uploaded Image')
@@ -52,6 +64,14 @@ def main():
                 proba = (1 - result.item()) * 100
 
         st.write(f"Prediction: {labels[pred]} ({proba:.2f}%)")
+
+def main():
+    pg = st.navigation([
+        st.Page(info_page, title="Home", icon=':material/home:'),
+        st.Page(prediction_page, title="Predict", icon=':material/stethoscope:'),
+    ])
+
+    pg.run()
 
 if __name__ == '__main__':
     main()
