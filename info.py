4360ff7b8b4867145ed0dad449cf938007523d95
Sujay Mann <92069576+SujayMann@users.noreply.github.com>
2025-01-05 11:36:24 +0530

    Update info.py

--- info.py
+++ info.py
@@ -11,5 +11,9 @@ def info_page():
         3. The model will predict if the x-ray shows pneumonia and display the result.
     """)
 
+    st.write("""If the app doesn't work, it is most likely due to unavailability of the model used for prediction.
+    The model file was uploaded using Git LFS free plan so if that exceeds the restrictions then the app might not work temporarily.
+    Apologies for the inconvenience caused.""")
+
 if __name__ == '__page__':
     info_page()
