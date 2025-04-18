0de65e34994cad5f1bd08285deb90430c86c6d57
Sujay Mann <92069576+SujayMann@users.noreply.github.com>
2025-04-18 19:11:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
 import streamlit as st
 
+st.set_page_config(
+    page_title='Chest X-Ray Classifier',
+    page_icon='🩺'
+)
+
 def main():
     pg = st.navigation([
         st.Page('info.py', title="Home", icon=':material/home:'),
